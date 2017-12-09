4ee1533d8d2997d6ccd4bf0af44909217d0838db
Blay09 <blay09@hotmail.de>
2017-12-09 11:18:40 +0100

    Add WIP overlay

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController instance;
+
+    public float levelTimer;
+
+    private bool isLevelRunning;
+
+    private void Awake() {
+        instance = this;
+
+        StartLevel();
+    }
+
+    private void Update() {
+        if (isLevelRunning) {
+            levelTimer += Time.deltaTime;
+        }
+    }
+
+    public void StartLevel() {
+        levelTimer = 0f;
+        isLevelRunning = true;
+    }
+
+}
---
+++ Assets/Scripts/OverlayCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OverlayCanvas : MonoBehaviour {
+
+    public float fadeInTime = 1f;
+
+    public Text timer;
+    public Image blackScreen;
+
+    private void Start() {
+        blackScreen.enabled = true;
+        blackScreen.canvasRenderer.SetAlpha(1f);
+        blackScreen.CrossFadeAlpha(0f, fadeInTime, true);
+    }
+
+    private void Update() {
+        timer.text = GameController.instance.levelTimer.ToString("00.00");
+    }
+
+}
