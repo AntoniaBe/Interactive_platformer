fe8d41148464eb89acf55d37e31d7365e04f2b5c
Antonia Berger <AntoniaBerger@gmx.net>
2017-12-09 14:10:18 +0100

    added function to detect if a hand is a fist or not

---
+++ Assets/DetectCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "hands")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Collision!");
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "hands")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Trigger!");
+        }
+    }
+
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class Test : MonoBehaviour {
+
+    LeapServiceProvider provider;
+    bool closedFist;
+
+    // Use this for initialization
+    void Start () {
+        provider = FindObjectOfType<LeapServiceProvider>() as LeapServiceProvider;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        Frame frame = provider.CurrentFrame;
+        foreach (Hand hand in frame.Hands)
+        {
+            if (!hand.Fingers[0].IsExtended && !hand.Fingers[1].IsExtended && !hand.Fingers[2].IsExtended && !hand.Fingers[3].IsExtended && !hand.Fingers[4].IsExtended)
+            {
+                closedFist = true;
+               // Debug.Log("Fist");
+            }
+
+            else {
+                closedFist = false;
+              //  Debug.Log("No Fist");
+            }
+        }
+    }
+}
