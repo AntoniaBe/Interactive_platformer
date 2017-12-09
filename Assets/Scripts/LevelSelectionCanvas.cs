fdfda4c2ad06d1a92f4e57451be1fdf4de565673
Blay09 <blay09@hotmail.de>
2017-12-09 11:19:15 +0100

    Add WIP level selection screen

---
+++ Assets/Scripts/LevelSelectionCanvas.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionCanvas : MonoBehaviour {
+
+    public float fadeOutTime = 2f;
+
+    private AsyncOperation asyncOperation;
+
+    public void SelectLevel(int level) {
+        print("Selected Levelaaa " + level);
+
+        var buttons = GetComponentsInChildren<Button>();
+        foreach (var button in buttons) {
+            button.enabled = false;
+        }
+
+        print("Selected Level " + level);
+
+        asyncOperation = SceneManager.LoadSceneAsync("Level" + level);
+        asyncOperation.allowSceneActivation = false;
+
+        StartCoroutine(HideScreen());
+    }
+
+    public IEnumerator HideScreen() {
+        var canvasGroup = GetComponent<CanvasGroup>();
+
+        while (canvasGroup.alpha > float.Epsilon) {
+            canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 0f, fadeOutTime);
+            yield return null;
+        }
+
+        asyncOperation.allowSceneActivation = true;
+    }
+
+}
