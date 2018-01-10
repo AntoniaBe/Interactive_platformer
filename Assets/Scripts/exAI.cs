ff4fcc6666e8b3297a1338824277d582c229a8fc
JennyLin1995 <eisundsahne@web.de>
2018-01-10 16:00:44 +0100

    modified jumping AI for better visuals, fixing no falling bug

--- Assets/Scripts/exAI.cs
+++ Assets/Scripts/exAI.cs
@@ -7,25 +7,22 @@ public class exAI : MonoBehaviour {
     private Rigidbody rb;
     public float speed = 2f;
     public float rayLenght = 3f;
-    private float jumpSpeed = 50f;
+    private float jumpSpeed = 4f;
     private RaycastHit vision;
     private Vector3 to = new Vector3(1f, 0f, 0f);
+    private float velY;
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        velY = rb.velocity.y;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(speed, 0, 0);
-
+        velY = rb.velocity.y;
         Debug.DrawRay(rb.transform.position, to * rayLenght, Color.red, 1.5f);
 
         if (Physics.Raycast(transform.position, to, out vision, rayLenght))
@@ -37,14 +34,22 @@ public class exAI : MonoBehaviour {
             {
 
                 Debug.Log("JUMP");
+                velY = rb.velocity.y;
                 rb.velocity = new Vector3(speed, jumpSpeed, 0);
 
             }
             else
             {
 
-                rb.velocity = new Vector3(speed, 0, 0);
+                rb.velocity = new Vector3(speed, velY, 0);
+
             }
+
+        }
+        else
+        {
+
+            rb.velocity = new Vector3(speed, velY, 0);
 
         }
     }
