7da2907407c5520b3f580afadf14fa9d9e7cf7f2
blay09 <blay09@hotmail.de>
2018-01-18 01:02:08 +0100

    Fix merge errors

--- Assets/Scripts/DetectCollision.cs
+++ Assets/Scripts/DetectCollision.cs
@@ -6,29 +6,24 @@ public class DetectCollision : MonoBehaviour {
 
     public bool collisionDetected;
 
-    void OnTriggerStay(Collider collider)
-    {
-                if (collider.gameObject.tag == "hands")
-        {
+    void OnTriggerEnter(Collider collider) {
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        if (collider.gameObject.tag == "hands") {
             //If the GameObject has the same tag as specified, output this message in the console
-
-           collisionDetected = true;
-           gameObject.GetComponent<Renderer>().material.color = Color.green;
-            //Debug.Log("COLLISION");
+            // Debug.Log("Collision!");
+            collisionDetected = true;
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
         }
-
-
     }
 
-    void OnTriggerExit(Collider collider)
-    {
-        if (collider.gameObject.tag == "hands")
-        {
+
+    void OnTriggerExit(Collider collider) {
+        if (collider.gameObject.tag == "hands") {
             //If the GameObject has the same tag as specified, output this message in the console
-           // Debug.Log("Collision Exit");
+            // Debug.Log("Collision Exit");
             collisionDetected = false;
             gameObject.GetComponent<Renderer>().material.color = Color.red;
-            //Debug.Log("NO COLLISION");
         }
     }
 
