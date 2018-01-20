7c58b8a7c0f8fba354de6f59875aa550aca9bde3
blay09 <blay09@hotmail.de>
2018-01-20 22:04:09 +0100

    Add dart trap that shoots arrows to push the NPC back Add some castle assets

---
+++ Assets/Scripts/DartTrap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DartTrap : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject dartPrefab;
+    public Vector3 minOffset;
+    public Vector3 maxOffset;
+    public float interval = 1f;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        StartCoroutine(FireDarts());
+    }
+
+    private IEnumerator FireDarts() {
+        while (true) {
+            yield return new WaitForSecondsRealtime(interval);
+            var dart = Instantiate(dartPrefab);
+            var offset = new Vector3(Random.Range(minOffset.x, maxOffset.x), Random.Range(minOffset.y, maxOffset.y), Random.Range(minOffset.z, maxOffset.z));
+            dart.transform.position = transform.position + offset;
+            dart.GetComponent<DartProjectile>().autoTarget = player;
+        }
+    }
+
+}
