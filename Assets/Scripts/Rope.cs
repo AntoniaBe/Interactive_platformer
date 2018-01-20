4ef7f9ff2153ea971a362574228ee5dbd5037fcf
blay09 <blay09@hotmail.de>
2018-01-21 00:23:26 +0100

    Start working on rope breaking logic

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rope : MonoBehaviour {
+
+    public bool isTopRobe;
+    public GameObject connectedRope;
+
+    private void OnTriggerEnter(Collider other) {
+        DetachRope();
+        connectedRope.GetComponent<Rope>().DetachRope();
+    }
+
+    public void DetachRope() {
+        if (!isTopRobe) {
+            transform.parent = null;
+            gameObject.AddComponent<Rigidbody>();
+        }
+    }
+
+}
