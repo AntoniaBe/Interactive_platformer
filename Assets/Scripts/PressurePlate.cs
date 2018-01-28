ee2c02c2e6289e437cdbcc19eb9529414fa722e3
blay09 <blay09@hotmail.de>
2018-01-28 19:58:13 +0100

    Castle Level Progress

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour {
+
+    public bool active;
+
+    private void OnCollisionEnter(Collision collision) {
+        active = true;
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        active = false;
+    }
+
+}
--- Assets/Scripts/SnappingController.cs
+++ Assets/Scripts/SnappingController.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SnappingController : MonoBehaviour {
 
+    public UnityEvent onSnapEvent;
     public GameObject[] grabbables;
+
+    private void Awake() {
+        if (onSnapEvent == null) {
+            onSnapEvent = new UnityEvent();
+        }
+    }
 
     private IEnumerator OnTriggerEnter(Collider col) {
         if (grabbables.Contains(col.gameObject)) {
@@ -12,7 +20,7 @@ public class SnappingController : MonoBehaviour {
             col.transform.rotation = transform.rotation;
             col.gameObject.layer = LayerMask.NameToLayer("SnapIn");
             col.GetComponent<Grabbable>().isSnappedIn = true;
-            //col.isTrigger = false;
+            onSnapEvent.Invoke();
             yield return new WaitForSeconds(0.5f);
             Destroy(gameObject);
         }
