cf428637fd47cd473eea4a245cd0da578c667772
blay09 <blay09@hotmail.de>
2018-01-30 14:19:13 +0100

    Import Castle Level decorations

---
+++ Assets/Scripts/BoxJump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxJump : MonoBehaviour {
+
+    public GameObject target;
+    
+	/// <summary>
+    /// Check if target object is in correct position, then set the objects tag to "jump"
+    /// </summary>
+	void Update () {
+        if (target.GetComponent<Grabbable>().isSnappedIn) {
+            gameObject.tag = "jump";
+        }
+    }
+}
