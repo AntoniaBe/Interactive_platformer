daa053098511df9e6d1a1e4df5e1aa3d997b9fe7
blay09 <blay09@hotmail.de>
2018-01-30 16:34:35 +0100

    Fix level issues and use BoxCast in jump to make it more robust

--- Assets/Scripts/exAI.cs
+++ Assets/Scripts/exAI.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class exAI : MonoBehaviour { // TODO rename to NPC or just AI or something
 
-    private Rigidbody rb;
     public float speed = 2f;
     public float knockbackRecovery = 3f;
-    public float rayLenght = 3f;
-    private float jumpSpeed = 4f;
-    private RaycastHit vision;
-    private Vector3 to = new Vector3(1f, 0f, 0f);
-    public Vector3 changedY = new Vector3(0f, -2f, 0f);
+    public float jumpSpeed = 4f;
+    public float jumpCastLength = 2.5f;
+    public Vector3 jumpCastOffset;
+    public Vector3 jumpCastSize = new Vector3(0.1f, 0.1f, 0.1f);
     public bool shouldWait;
 
+    private Rigidbody rigidBody;
     private Vector3 knockback;
 
     private void Start() {
-        rb = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     private void Update() {
@@ -26,16 +25,19 @@ public class exAI : MonoBehaviour { // TODO rename to NPC or just AI or somethin
     }
 
     private void FixedUpdate() {
-        Debug.DrawRay(rb.transform.position - changedY, to * rayLenght, Color.red, 1.5f);
+        //Debug.DrawRay(rb.transform.position - changedY, o * rayLenght, Color.red, 1.5f);
+        Debug.DrawRay(transform.position + jumpCastOffset - jumpCastSize, Vector3.right * jumpCastLength, Color.red, 1.5f);
+        Debug.DrawRay(transform.position + jumpCastOffset + jumpCastSize, Vector3.right * jumpCastLength, Color.red, 1.5f);
 
-        Vector3 targetVelocity = new Vector3(0, rb.velocity.y, 0);
+        Vector3 targetVelocity = new Vector3(0, rigidBody.velocity.y, 0);
 
         if(!shouldWait) {
             // Keep walkinng to the right if not instructed to wait
             targetVelocity.x = speed;
 
             // Jump when about to run against a grabbable, snapped in object
-            if (Physics.Raycast(transform.position - changedY, to, out vision, rayLenght, 1 << LayerMask.NameToLayer("SnapIn"), QueryTriggerInteraction.Ignore)) {
+            if (Physics.BoxCast(transform.position + jumpCastOffset, jumpCastSize, Vector3.right, Quaternion.identity, jumpCastLength, 1 << LayerMask.NameToLayer("SnapIn"))) {
+            // if (Physics.Raycast(transform.position - changedY, to, out vision, rayLenght, 1 << LayerMask.NameToLayer("SnapIn"), QueryTriggerInteraction.Ignore)) {
                 targetVelocity.y = jumpSpeed;
             }
         }
@@ -43,7 +45,7 @@ public class exAI : MonoBehaviour { // TODO rename to NPC or just AI or somethin
         // Apply knockback to the final velocity
         targetVelocity += knockback;
 
-        rb.velocity = targetVelocity;
+        rigidBody.velocity = targetVelocity;
     }
 
     public void Knockback(Vector3 knockback) {
