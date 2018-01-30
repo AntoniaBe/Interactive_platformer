70035efc8572aad645f98c409a768882225b1bf2
blay09 <blay09@hotmail.de>
2018-01-30 01:20:22 +0100

    Reapply fixes that got broken by the level import

--- Assets/Scripts/SnappingController.cs
+++ Assets/Scripts/SnappingController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SnappingController : MonoBehaviour {
 
-
-
+    public UnityEvent onSnapEvent;
     public GameObject[] grabbables;
+
+    private void Awake() {
+        if (onSnapEvent == null) {
+            onSnapEvent = new UnityEvent();
+        }
+    }
 
     private IEnumerator OnTriggerEnter(Collider col) {
         if (grabbables.Contains(col.gameObject)) {
@@ -15,7 +21,7 @@ public class SnappingController : MonoBehaviour {
 
             col.gameObject.layer = LayerMask.NameToLayer("SnapIn");
             col.GetComponent<Grabbable>().isSnappedIn = true;
-            col.isTrigger = false;
+            onSnapEvent.Invoke();
             yield return new WaitForSeconds(0.5f);
             Destroy(gameObject);
         }
